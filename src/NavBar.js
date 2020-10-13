675af0cbfd643ec313597bf6897dd59987a0a18e
James Gutowski <gutowskj@gmail.com>
2020-10-12 18:02:58 -0600

    Navbar font added and icon centered

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,10 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function NavBar(props) {
   return (
     <div>
-    <nav className="navbar navbar-dark bg-primary">
-      <a className="navbar-brand" href="#">
-        <img src="./images/half-full-white.png" width="30" height="30" style={{marginRight: '15px'}} className="d-inline-block align-top" alt="" loading="lazy"></img>
-          TriDri
+    <nav className="navbar  navbar-dark bg-primary">
+      <a className="navbar-brand navtext" href="#">
+        <img src="./images/half-full-white.png" width="30" height="30" style={{marginRight: '15px', marginTop: "4px"}} className="d-inline-block align-top" alt="" loading="lazy"></img>
+          Tri Dri
       </a>
     </nav>
     </div>
